501b2ba3d3dfbafa8c55048474eb67888cd537a1
MNewbigging <mrnewbigging@gmail.com>
2021-10-07 20:08:02 +0100

    blueprint setup

--- src/components/details-panel/DetailsPanel.tsx
+++ src/components/details-panel/DetailsPanel.tsx
@@ -1,9 +1,15 @@
+import { observer } from 'mobx-react';
 import React from 'react';
 
 import './details-panel.scss';
 
+@observer
 export class DetailsPanel extends React.Component {
   public render() {
     return <div className={'details-panel'}></div>;
+  }
+
+  private renderNoGridPlanCta() {
+    return <div></div>;
   }
 }
