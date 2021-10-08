b045650f431c6713c15f7e205827ec468a7c03ad
MNewbigging <mrnewbigging@gmail.com>
2021-10-08 18:43:38 +0100

    entry dialog

--- src/components/GridPlanner.tsx
+++ src/components/GridPlanner.tsx
@@ -6,11 +6,17 @@ import { GridZone } from './grid-zone/GridZone';
 import { VerticalNavbar } from './navbar/VerticalNavbar';
 
 import './grid-planner.scss';
+import { Button, Dialog, Icon, Intent } from '@blueprintjs/core';
 
 export class GridPlanner extends React.Component {
   private readonly gridPlannerState = new GridPlannerState();
 
   public render() {
+    // Show the entry dialog if there is no grid plan
+    if (!this.gridPlannerState.gridPlan) {
+      return this.renderEntryDialog();
+    }
+
     return (
       <div className={'grid-planner'}>
         <div className={'navbar-area'}>
@@ -22,6 +28,22 @@ export class GridPlanner extends React.Component {
         <div className={'grid-zone-area'}>
           <GridZone />
         </div>
+      </div>
+    );
+  }
+
+  private renderEntryDialog() {
+    return (
+      <div className={'grid-planner-entry'}>
+        <Dialog isOpen={true}>
+          <div className={'entry-dialog'}>
+            <Icon icon={'layers'} size={50} />
+            <h3>Grid Planner</h3>
+            <p>Create a new grid, or choose a file to load</p>
+            <Button icon={'add'} text={'Create grid plan'} intent={Intent.PRIMARY} />
+            <Button icon={'document-open'} text={'Load grid plan'} disabled />
+          </div>
+        </Dialog>
       </div>
     );
   }
